603a07896d894504be35b30370508f7ec7220e17
scriptpythonic <Quadribasit04@gmail.com>
2025-04-18 16:54:51 +0100

    make the change according to mr jibbs

--- src/api/AxiosInstance.tsx
+++ src/api/AxiosInstance.tsx
@@ -4,7 +4,9 @@ const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': true,
     },
+    withCredentials: true,
   });
 
 export default axiosInstance;
